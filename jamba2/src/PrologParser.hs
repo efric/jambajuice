6f4f414ab2f69b45a917b1b22b24755ed52f6bca
Eric Feng <ef2648@columbia.edu>
2023-05-08 01:59:41 -0400

    parser: initial parser for prolog output

---
+++ jamba2/src/PrologParser.hs
@@ -0,0 +1,57 @@
+module PrologParser where
+
+import qualified Data.Map as M
+import qualified Data.Text.Lazy as L
+import Text.Parsec
+    ( alphaNum,
+      char,
+      space,
+      endBy,
+      (<|>),
+      many,
+      parse,
+      ParseError,
+      try,
+      ParsecT,
+      endOfLine, digit, optional )
+import Text.Parsec.Text.Lazy (Parser)
+import qualified Data.Functor.Identity
+
+data Type = Type String | Arrow Type Type deriving Show
+
+singletype :: Parser Type
+singletype = do
+    types <- many alphaNum
+    return $ Type types
+
+arrow :: Parser Type
+arrow = do
+    input <- many alphaNum
+    char ','
+    output <- try arrowtype <|> singletype
+    return $ Arrow (Type input) output
+
+arrowtype :: Parser Type
+arrowtype = do
+    char '['
+    types <- arrow
+    char ']'
+    return types
+
+types :: Parser Type
+types = try arrowtype <|> singletype
+
+line :: Parser (M.Map Integer Type)
+line = do
+    node <- many alphaNum *> char '_' *> many digit
+    space
+    M.singleton (read node) <$> types
+
+prolog :: ParsecT
+  L.Text () Data.Functor.Identity.Identity (M.Map Integer Type)
+prolog = do 
+    res <- endBy line (optional endOfLine)
+    pure $ M.unions res
+
+parseProlog :: L.Text -> Either ParseError (M.Map Integer Type)
+parseProlog = parse prolog ""
